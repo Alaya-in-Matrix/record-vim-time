547768a495b9c13f98b835db75ea00f0612ac443
lvwenlong <lvwenlong_lambda@qq.com>
2015-08-04 11:26:14 +0800

    init commit

---
+++ src/Main.hs
@@ -0,0 +1,8 @@
+module Main where
+
+import Control.Monad
+import Control.Applicative hiding((<|>),many,optional)
+import Text.ParserCombinators.Parsec
+import System.Environment
+
+main = undefined
