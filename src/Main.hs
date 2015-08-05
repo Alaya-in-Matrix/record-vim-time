860e68d3d49e45187c4eea6e369f370242f7c107
lvwenlong <lvwenlong_lambda@qq.com>
2015-08-05 20:07:45 +0800

    type for flags

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,39 @@
+{-#language TypeSynonymInstances #-}
+{-#language FlexibleInstances #-}
 module Main where
 
 import Control.Monad
 import Control.Applicative hiding((<|>),many,optional)
 import Text.ParserCombinators.Parsec
 import System.Environment
+import Data.Default
 
-main = undefined
+newtype Email   = Email String deriving(Eq, Show)
+newtype LogPath = LogPath String deriving(Eq, Show)
+instance Default Email where
+    def = Email ""
+instance Default LogPath where 
+    def = LogPath "~/activity"
+data Flags = Flags {
+    email :: Email
+  , logPath :: LogPath 
+} deriving(Eq, Show)
+
+
+cvt :: [a] -> [(a,a)]
+cvt []  = []
+cvt [x] = []
+cvt (x:y:xs) = (x,y) : cvt xs
+
+defMaybe :: (Default a) => (Maybe a) -> a
+defMaybe Nothing  = def
+defMaybe (Just v) = v
+
+getFlags :: [(String,String)] -> Flags
+getFlags args = let email = defMaybe $ lookup "-e" args
+                    path  = defMaybe $ lookup "-d" args
+                 in Flags (Email email) (LogPath path)
+main = do
+    args  <- (liftM cvt) getArgs
+    flags <- return $ getFlags args
+    print flags
