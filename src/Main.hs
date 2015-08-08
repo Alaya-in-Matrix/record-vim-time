9cf8f46fe56f978490ccbdc1b4e3a22c76d34b5b
lvwenlong <lvwenlong_lambda@qq.com>
2015-08-08 19:12:10 +0800

    Demo finished

--- src/Main.hs
+++ src/Main.hs
@@ -1,53 +1,14 @@
 module Main where
 
 import Control.Monad
-import Control.Applicative hiding((<|>),many,optional)
 import Text.ParserCombinators.Parsec
 import System.Environment
-import Data.Default
 import Data.Time.Clock
+import Data.Time.LocalTime
 import Data.Time.Calendar
-
-newtype Email   = Email String deriving(Eq, Show)
-newtype LogPath = LogPath String deriving(Eq, Show)
-
-getEmail :: Email -> String
-getEmail (Email e) = e
-getLogPath :: LogPath -> String
-getLogPath (LogPath p) = p
-instance Default Email where
-    def = Email ""
-instance Default LogPath where 
-    def = LogPath "~/activity"
-data Flags = Flags {
-    email   :: Email
-  , logPath :: LogPath 
-} deriving(Eq, Show)
-
-secondOfDay :: VimLogTime -> Integer
-secondOfDay t = let h = toInteger $ vimLogHour   t
-                    m = toInteger $ vimLogMinute t
-                    s = toInteger $ vimLogSecond t
-                 in h * 3600 + m * 60 + s
-
-pairFold :: [a] -> [(a,a)]
-pairFold []  = []
-pairFold [x] = []
-pairFold (x:y:xs) = (x,y) : pairFold xs
-
-defMaybe :: (Default a) => Maybe a -> a
-defMaybe Nothing  = def
-defMaybe (Just v) = v
-
-getFlags :: [(String,String)] -> Flags
-getFlags args = let email = defMaybe $ lookup "-e" args
-                    path  = defMaybe $ lookup "-d" args
-                 in Flags (Email email) (LogPath path)
-
-date :: IO String -- (year, month, day)
-date = liftM (map replace . showGregorian . utctDay) getCurrentTime
-    where replace '-' = '/'
-          replace  c   = c
+import Data.List
+import Data.List.Split hiding(endBy)
+import qualified Data.Map as Map
 
 data VimLogTime = VimLogTime {
     vimLogYear   :: Integer
@@ -57,25 +18,35 @@ data VimLogTime = VimLogTime {
   , vimLogMinute :: Int
   , vimLogSecond :: Int
 } deriving(Eq, Show)
-
 data VimLogAction = Create | Open | Write deriving(Eq, Show)
 type VimLogGitBranch = String
-
 data VimLog = VimLog {
     time       :: VimLogTime
   , action     :: VimLogAction
   , editedfile :: FilePath
   , gitbranch  :: Maybe VimLogGitBranch
-} deriving(Eq, Show)
+} deriving(Eq)
+instance Show VimLog where 
+    show (VimLog t a e g) = "VimLog {\n    " ++ show t ++ "\n    " ++ show a ++ "\n    " ++ e ++ "\n    " ++ show g ++ "\n}"
 
-delimiter :: Parser Char
-delimiter = char ';'
+secondOfDay :: VimLogTime -> Integer
+secondOfDay t = let h = toInteger $ vimLogHour   t
+                    m = toInteger $ vimLogMinute t
+                    s = toInteger $ vimLogSecond t
+                 in h * 3600 + m * 60 + s
+
+dateToday :: IO String -- (year, month, day)
+dateToday = liftM (map replace . showGregorian . utctDay) getCurrentTime
+    where replace '-' = '/'
+          replace  c   = c
 
 logParser :: Parser VimLog
 logParser = VimLog <$> (logTimeParser  <* delimiter)
                    <*> (actionParser   <* delimiter)
                    <*> (filePathParser <* delimiter)
                    <*> branchParser
+   where delimiter = char ';'
+
 logTimeParser :: Parser VimLogTime
 logTimeParser = VimLogTime <$> (read <$> many1   digit <* char '-')     -- year
                            <*> (read <$> count 2 digit <* char '-')     -- month
@@ -93,19 +64,13 @@ filePathParser :: Parser FilePath
 filePathParser = try quotedPath <|> many1 (noneOf ";\n\r")
 
 quotedPath :: Parser FilePath
-quotedPath = do
-    char '"'
-    path <- many1 quotedChar
-    char '"'
-    return path
+quotedPath = char '"' *> many1 quotedChar <* char '"'
 
 quotedChar :: Parser Char
-quotedChar = try ('$' <$ string "\\\"") <|>  noneOf "\"" <?> "fuck"
-
+quotedChar = try ('"' <$ string "\\\"") <|>  noneOf "\"" <?> "fuck"
 
 branchParser :: Parser (Maybe VimLogGitBranch)
 branchParser = (Just <$> filePathParser) <|> (Nothing <$ string "")
-
 
 eol :: Parser String 
 eol = try (string "\n\r")
@@ -114,15 +79,41 @@ eol = try (string "\n\r")
   <|> string "\n"
   <?> "end of line"
 
+logFileParser :: Parser [VimLog]
 logFileParser = (logParser `endBy` eol) 
 
+
+logMap :: [VimLog] -> Map.Map FilePath [(Integer, VimLogAction)]
+logMap logs = logMap' (Map.empty) logs
+    where logMap' m [] = m
+          logMap' m (x:xs) = logMap' m' xs
+              where m' = let t = (secondOfDay.time) x
+                             a = action x
+                             f = editedfile x
+                             pair = (t,a)
+                          in case (Map.lookup f m) of
+                               Nothing  -> Map.insert f (pair:[]) m
+                               Just rec -> Map.update (\_ -> Just (pair:rec)) f m
+
+
+trans2 :: (a->a->b)->(c->a)->(c->c->b)
+trans2 func cvt c1 c2 = func (cvt c1) (cvt c2)
+
+duration :: [(Integer,VimLogAction)] -> Integer
+duration logs = let sorted  = sortBy (compare `trans2` fst) logs
+                    splited = splitWhen ((/= Write) . snd) sorted
+               in sum $ map calcDura splited
+                   where calcDura [] = 0
+                         calcDura xs = (fst $ last xs) - (fst $ head xs)
+
+main :: IO ()
 main = do
-    args       <- liftM pairFold getArgs
-    date       <- date
-    let flags = getFlags args
-        logRootDir = (getLogPath . logPath) flags
-        path       = logRootDir ++ "/" ++ date ++ ".log"
+    path   <- (foldr (++) "") <$> (sequence [liftM head getArgs , pure "/" ,dateToday ,pure ".log"])
     parsed <- parseFromFile logFileParser path 
     case parsed of
       Left errMsg -> putStrLn $ show errMsg
-      Right val   -> mapM_ (putStrLn . show) val
+      Right val   -> let lm = logMap $ reverse val
+                         dm = Map.map duration lm
+                         vimtime = timeToTimeOfDay $ secondsToDiffTime $ foldr (+) 0 $  map snd (Map.toList dm)
+                      in putStrLn $ "Time you spent on VIM today: " ++ show vimtime
+
